619a78faf4b16ea3333d6ad3f4cdecfff237cf50
Aldo Utrera <aldo.utrera@travelogyx.com>
2015-04-01 10:30:08 -0600

    Using the false boolean

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "Game.h"
 
 int main(int argc, char* argv[]) {
-  game_init("Title", 100, 100, 640, 480, 0);
+  game_init("Title", 100, 100, 640, 480, false);
 
   while(is_game_running()) {
     game_handle_events();
